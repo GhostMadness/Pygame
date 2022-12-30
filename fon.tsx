e0f221bd988a73338295abbeb263bc31ebda0050
GhostMadness <ghostmadness55@gmail.com>
2022-12-30 19:31:19 +0300

    Create fon.tsx

---
+++ fon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="fon" tilewidth="32" tileheight="32" tilecount="3600" columns="80">
+ <image source="qwe/fon.jpg" width="2560" height="1440"/>
+</tileset>
