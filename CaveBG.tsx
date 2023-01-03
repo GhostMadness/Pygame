da2b9351cd9eac41ff279e9ade6c56ebde30ed37
GhostMadness <ghostmadness55@gmail.com>
2023-01-03 13:40:46 +0300

    Delete CaveBG.tsx

--- CaveBG.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="CaveBG" tilewidth="32" tileheight="32" tilecount="576" columns="32">
- <image source="CaveBG.png" width="1024" height="576"/>
-</tileset>
