e23fa3eb7718edf339becd9e916c3a8bdd9d9299
Z3N1TS <likeduple@gmail.com>
2023-01-04 10:33:02 +0300

    eas

---
+++ location_4/house_tile_2033.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="house_tile_2033" tilewidth="40" tileheight="40" tilecount="361" columns="19">
+ <image source="house_tile.png" width="768" height="768"/>
+</tileset>
