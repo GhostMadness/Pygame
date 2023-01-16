cc5f44f7c58bfc3268aa8b7c79cc295c468fcb91
Z3N1TS <likeduple@gmail.com>
2023-01-16 11:39:43 +0300

    dsrfg

---
+++ location_6/skeleton.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="skeleton" tilewidth="32" tileheight="32" tilecount="400" columns="20">
+ <image source="skeleton.png" width="640" height="640"/>
+</tileset>
